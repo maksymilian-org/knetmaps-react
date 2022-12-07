68e68a9256bf17c019b745749442b90d17ebbb9f
Maksymilian Furtak <maksymilian.furtak@embiq.com>
2022-12-07 12:35:30 +0100

    Run app from script tag

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const KNETMAPS: any = (window.KNETMAPS = {});
+
+KNETMAPS.KnetMaps = () => ({
+  draw: (container: string, config?: any) => {
+    const root = ReactDOM.createRoot(
+      document.querySelector(container) as HTMLElement
+    );
+    root.render(
+      <React.StrictMode>
+        <App config={config} />
+      </React.StrictMode>
+    );
+  },
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
