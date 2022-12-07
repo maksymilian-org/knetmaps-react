db0dc265e679f74067f9ff4568bd0054c0b3a9d6
Maksymilian Furtak <maksymilian.furtak@embiq.com>
2022-12-07 16:39:13 +0100

    Clean the code

--- src/components/graph/Graph.tsx
+++ src/components/graph/Graph.tsx
@@ -2,10 +2,10 @@ import React, { FC } from "react";
 import { useGraph } from "./useGraph";
 import styles from './Graph.module.scss';
 
-const Graph: FC = (props) => {
-  const { text, containerRef } = useGraph();
+const Graph: FC = () => {
+  const { containerRef } = useGraph();
 
-  return <div className={styles.root} ref={containerRef}>{text}</div>;
+  return <div className={styles.root} ref={containerRef}></div>;
 };
 
 export default Graph;
